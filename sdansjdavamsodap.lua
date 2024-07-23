418d84d71744846f12689b7f058225fd4a09b71f
ground <161968536+groundded@users.noreply.github.com>
2024-07-24 03:10:29 +0400

    Update sdansjdavamsodap.lua

--- sdansjdavamsodap.lua
+++ sdansjdavamsodap.lua
@@ -1,12 +1,1 @@
-local HWIDTable = loadstring(game:HttpGet("https://raw.githubusercontent.com/groundded/schemabs/main/shdbahvamasdasdiwawhitelis.lua"))() -- We are making a variable for where your HWID / ClientID table is going to be located. (You can use pastebin / github)
 
-local HWID = game:GetService("RbxAnalyticsService"):GetClientId()
-
-
-for i, v in pairs(HWIDTable) do
-    if v == HWID then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/groundded/schemabs/main/sdgajhsdvghjwcvkashdvwjhkachfgci13y21ckasdasdasd"))()
-    elseif v ~= HWID then
-        print("The HWID is not Whitelisted. HWID: ", HWID)
-    end
-end
