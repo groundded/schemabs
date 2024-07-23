5ce097487860a794e29b291c53b23c2996cd8057
ground <161968536+groundded@users.noreply.github.com>
2024-07-23 20:09:19 +0400

    Update sdansjdavamsodap.lua

--- sdansjdavamsodap.lua
+++ sdansjdavamsodap.lua
@@ -1,4 +1,4 @@
-local HWIDTable = loadstring(game:HttpGet("https://raw.githubusercontent.com/groundded/1h2v3hj21v4gv21hg3h12v3g12v3gh12g3c1h2c/main/k1v3hj1v231v2jhg3cv1k2hg3ch1g2c3hg1c23cg1k23ghj12c3.lua"))() -- We are making a variable for where your HWID / ClientID table is going to be located. (You can use pastebin / github)
+local HWIDTable = loadstring(game:HttpGet("https://raw.githubusercontent.com/groundded/schemabs/main/shdbahvamasdasdiwawhitelis.lua"))() -- We are making a variable for where your HWID / ClientID table is going to be located. (You can use pastebin / github)
 
 local HWID = "hwid here" -- NOTE IMPORTANT: I didnt put my HWID / ClientID for privacy reasons. You should replace this with game:GetService("RbxAnalyticsService"):GetClientId()
 
